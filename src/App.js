93b41dfe243299b82f0dd237afa02d5c1334075c
Syed Muhammed Bilal <intergo878@gmail.com>
2020-04-30 16:32:21 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import NavbarPage from './components/navbar.component'
+
+function App() {
+  return (
+    <div>
+      <NavbarPage />
+    </div>
+  );
+}
+
+export default App;
